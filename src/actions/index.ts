7099c9fce96d09534cdcbd7e5db917d3c99200bd
Marco Lancellotti <marcolanciesn@gmail.com>
2020-03-25 11:22:30 +0100

    added interface to dispatch

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,9 +8,14 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodoAction {
+  payload: Todo[];
+  type: ActionTypes;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-export const fetchToDo = () => async (dispatch: Dispatch) => {
+export const fetchToDo = () => async (dispatch: Dispatch<FetchTodoAction>) => {
   const response = await axios.get<Todo[]>(url);
 
   dispatch({ type: ActionTypes.fetchTodos, payload: response.data });

664a9ad5d4b41e71e2613cc5b1577f13e357a2a3
Marco Lancellotti <marcolanciesn@gmail.com>
2020-03-25 12:36:26 +0100

    wired up delete action

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ReduxState } from '../reducers';
-import { fetchToDos, Todo } from '../actions';
+import { fetchToDos, deleteTodo, Todo } from '../actions';
 interface AppProps {
   todos: Todo[];
   fetchToDos: () => void;
+  deleteTodo: (id: number) => void;
 }
 
 class App extends React.Component<AppProps> {
   state = { counter: 0 };
-
+  deleteIconStyle = { color: 'red', cursor: 'pointer' };
   onButtonClick = (): void => {
     this.props.fetchToDos();
+  };
+
+  onDeleteClick = (id: number): void => {
+    this.props.deleteTodo(id);
   };
 
   listTodo = (todos: Todo[]): JSX.Element => (
     <ul>
       {todos.map(({ id, title }) => (
-        <li key={id}>{title}</li>
+        <li key={id}>
+          {title}{' '}
+          <span>
+            <i
+              className="fas fa-trash"
+              style={this.deleteIconStyle}
+              onClick={() => this.onDeleteClick(id)}
+            />
+          </span>
+        </li>
       ))}
     </ul>
   );
@@ -35,5 +49,5 @@ const mapStateToProps = (state: ReduxState): { todos: Todo[] } => {
   const { todos } = state;
   return { todos };
 };
-const mapDispatchToProps = { fetchToDos };
+const mapDispatchToProps = { fetchToDos, deleteTodo };
 export default connect(mapStateToProps, mapDispatchToProps)(App);

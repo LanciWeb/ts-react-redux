53f72697ae7fde0447caa914559ddf6e554476b8
Marco Lancellotti <marcolanciesn@gmail.com>
2020-03-25 10:38:00 +0100

    added Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import ReactDOM from 'react-dom';
-interface AppProps {
-  color?: string;
-}
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+const store = createStore(reducers, applyMiddleware(thunk));
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App color="red" />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App color="red" />
+  </Provider>,
+  document.getElementById('root')
+);
